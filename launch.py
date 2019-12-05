4ca32f70eb1d88ac7253b44aa68a8248567f13b7
HigashiKed <eastkin0815@gmail.com>
2019-12-05 05:49:59 +0000

    launch.py の変更

--- launch.py
+++ launch.py
@@ -3,12 +3,12 @@ from configparser import ConfigParser
 
 from swisscom_ai.research_keyphrase.embeddings.emb_distrib_local import EmbeddingDistributorLocal
 from swisscom_ai.research_keyphrase.model.input_representation import InputTextObj
-from swisscom_ai.research_keyphrase.model.method import MMRPhrase
-from swisscom_ai.research_keyphrase.preprocessing.postagging import PosTaggingCoreNLP
+from swisscom_ai.research_keyphrase.model.method import MMRPhrase,MMRSent
+from swisscom_ai.research_keyphrase.preprocessing.postagging import PosTaggingSpacy
 from swisscom_ai.research_keyphrase.util.fileIO import read_file
 
 
-def extract_keyphrases(embedding_distrib, ptagger, raw_text, N, lang, beta=0.55, alias_threshold=0.7):
+def extract_keyphrases(embedding_distrib, ptagger, raw_text, N, lang, method='MMRSent', beta=0.55, alias_threshold=0.7):
     """
     Method that extract a set of keyphrases
 
@@ -24,9 +24,18 @@ def extract_keyphrases(embedding_distrib, ptagger, raw_text, N, lang, beta=0.55,
     2)list of associated relevance scores (list of float)
     3)list containing for each keyphrase a list of alias (list of list of string)
     """
-    tagged = ptagger.pos_tag_raw_text(raw_text)
+    tagged = ptagger.pos_tag_raw_text(raw_text) #taggedは、入力された文
+    print("finished tagged")
+    #print(tagged)
     text_obj = InputTextObj(tagged, lang)
-    return MMRPhrase(embedding_distrib, text_obj, N=N, beta=beta, alias_threshold=alias_threshold)
+    print("finished InputTextObj")
+    #return MMRPhrase(embedding_distrib, text_obj, N=N, beta=beta, alias_threshold=alias_threshold)
+    if(method == 'MMRSent'):
+        return MMRSent(embedding_distrib, text_obj, N=N, beta=beta)
+    elif(method == 'MMRPhrase'):
+        return MMRPhrase(embedding_distrib, text_obj, N=N, beta=beta, alias_threshold=alias_threshold)
+    else:
+        pass
 
 
 def load_local_embedding_distributor():
@@ -39,9 +48,16 @@ def load_local_embedding_distributor():
 def load_local_corenlp_pos_tagger():
     config_parser = ConfigParser()
     config_parser.read('config.ini')
-    host = config_parser.get('STANFORDCORENLPTAGGER', 'host')
-    port = config_parser.get('STANFORDCORENLPTAGGER', 'port')
+#    host = config_parser.get('STANFORDCORENLPTAGGER', 'host')
+    host = "172.20.0.3"
+    #port = config_parser.get('STANFORDCORENLPTAGGER', 'port')
+    port = "9000"
     return PosTaggingCoreNLP(host, port)
+
+
+def load_spacy_tagger():
+    tagger = PosTaggingSpacy()
+    return PosTaggingSpacy()
 
 
 if __name__ == '__main__':
@@ -52,8 +68,7 @@ if __name__ == '__main__':
     group.add_argument('-text_file', help='file containing the raw text to process')
     
 
-    parser.add_argument('-tagger_host', help='CoreNLP host', default='localhost')
-    parser.add_argument('-tagger_port', help='CoreNLP port', default=9000)
+    #parser.add_argument('-tagger_host', help='CoreNLP host', default='localhost')
     parser.add_argument('-N', help='number of keyphrases to extract', required=True, type=int)
     args = parser.parse_args()
 
@@ -63,5 +78,6 @@ if __name__ == '__main__':
         raw_text = args.raw_text
 
     embedding_distributor = load_local_embedding_distributor()
-    pos_tagger = load_local_corenlp_pos_tagger(args.tagger_host, args.tagger_port)
-    print(extract_keyphrases(embedding_distributor, pos_tagger, raw_text, args.N, 'en'))
+    # pos_tagger = load_local_corenlp_pos_tagger(args.tagger_host, args.tagger_port)
+    pos_tagger = load_spacy_tagger()
+    #print(extract_keyphrases(embedding_distributor, pos_tagger, raw_text, args.N, 'en'))
